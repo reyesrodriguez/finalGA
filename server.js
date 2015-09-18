72fba1b641952bc9063695d05dc95da6d1264eab
reyes rodriguez rodriguez <reyesorodriguez@gmail.com>
2015-09-18 18:51:56 -0400

    xh access...

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ router.get('/', function(req, res) {
 });
 
 app.use( function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
