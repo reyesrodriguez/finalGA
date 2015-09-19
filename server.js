2178c4025a8a8abac6c19fdffb7b77f2f17888a9
reyes rodriguez rodriguez <reyesorodriguez@gmail.com>
2015-09-19 02:07:46 -0400

    access error

--- server.js
+++ server.js
@@ -50,10 +50,25 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-app.use( function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    'use strict';
+
+    res.header('Access-Control-Allow-Origin', '*');
+
+    if (req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    }
+    if (req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    }
+
+    res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else  {
+        next();
+    }
 });
   
   app.get('/api/entities', function(req, res) {
